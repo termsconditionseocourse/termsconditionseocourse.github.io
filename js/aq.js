bcd4455dfcf1c31748275cbc12052ab9ab8541fc
termsconditionseocourse <56712213+termsconditionseocourse@users.noreply.github.com>
2019-10-18 11:19:54 +0530

    Add files via upload

---
+++ js/aq.js
@@ -0,0 +1,13 @@
+   /**
+     * EFECTO PARA FLECHAS EN ACORDEON
+     */
+    
+    $(document).on('show','.accordion', function (e) {
+         //$('.accordion-heading i').toggleClass(' ');
+         $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+    });
+    
+    $(document).on('hide','.accordion', function (e) {
+        $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+        //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+    });
